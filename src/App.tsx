dc3140f6f0aa8ab7028dc6adae6f9d9b780c8113
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-06-09 12:36:32 +0300

    part 1 episode 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss'
+import './styles/index.scss'
 import {Routes, Route, Link} from "react-router-dom";
-import MainPage from "./pages/MainPage/MainPage";
-import AboutPage from "./pages/AboutPage/AboutPage";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Routes>
-                <Route path={'/'} element={<MainPage />}/>
-                <Route path={'/about'} element={<AboutPage />}></Route>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                        <Route path={'/'} element={<MainPageAsync />}/>
+                        <Route path={'/about'} element={<AboutPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,10 @@
+import {lazy} from "react";
+
+export const MainPageAsync = lazy(() =>
+    new Promise((resolve) =>
+        setTimeout(() => {
+            // @ts-ignore
+            resolve(import('./MainPage'))
+        }, 1500)
+    )
+);
